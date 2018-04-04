f16e20586eec1b2b60d0e7880de4e731053e9a66
yuriy_beloff <paganismrus@gmail.com>
2018-04-05 00:06:54 +0300

    completed

--- README.md
+++ README.md
@@ -1,6 +1,21 @@
 # Night Owls Detector
 
-[TODO. There will be project description]
+Скрипт выводит пользователей devman, которые отправили задачи на проверку после 24:00 и до 06:00
+
+# Как запустить
+
+Скрипт требует для своей работы установленного интерпретатора Python версии 3.5
+
+Запуск на Linux:
+
+```bash
+pip install -r requirements.txt # alternatively try pip3
+
+$ python3.5 seek_dev_nighters.py
+
+hoholsaniy, paganismrus, oeananas, АлексейЛескин, maxdmtrev
+
+```
 
 # Project Goals
 
--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,33 @@
-def load_attempts():
-    pages = 1
-    for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
-        }
+import requests
+import pytz
+from datetime import time, datetime
 
-def get_midnighters():
-    pass
+PATH = 'http://devman.org/api/challenges/solution_attempts/'
+
+
+def load_attempts():
+    pages = requests.get(PATH).json()['number_of_pages']
+    users_data = []
+    for page in range(1, pages + 1):
+        payload = {'page': page}
+        load_data = requests.get(PATH, params=payload).json()
+        users_data.extend(load_data['records'])
+    return users_data
+
+
+def get_midnighters(users):
+    midnight = time(hour=0, minute=0, microsecond=0)
+    morning = time(hour=6, minute=0, microsecond=0)
+    users_midnighters = []
+    for user in users:
+        user_time_zone = pytz.timezone(user['timezone'])
+        local_dt = user_time_zone.localize(
+            datetime.fromtimestamp(user['timestamp']))
+
+        if midnight < local_dt.time() < morning:
+            users_midnighters.append(user['username'])
+    return set(users_midnighters)
+
 
 if __name__ == '__main__':
-  pass
+    print(get_midnighters(load_attempts()))
