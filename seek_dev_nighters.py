3ce60195b8c88446dbf9e3beaf5b50177c8ceccc
yuriy_beloff <paganismrus@gmail.com>
2018-04-06 15:09:26 +0300

    deleted global variables and modified script using while

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,33 +1,44 @@
 import requests
 import pytz
-from datetime import time, datetime
-
-PATH = 'http://devman.org/api/challenges/solution_attempts/'
+from datetime import datetime
 
 
 def load_attempts():
-    pages = requests.get(PATH).json()['number_of_pages']
-    users_data = []
-    for page in range(1, pages + 1):
-        payload = {'page': page}
-        load_data = requests.get(PATH, params=payload).json()
-        users_data.extend(load_data['records'])
-    return users_data
+    URL = 'http://devman.org/api/challenges/solution_attempts/'
+    page = 1
+    payload = {'page': page}
+
+    while True:
+        try:
+            response = requests.get(URL, params=payload).json()
+            payload = {'page': page}
+            page += 1
+            users_attempts = response['records']
+            for attempt in users_attempts:
+                yield attempt
+        except ValueError:
+            break
 
 
 def get_midnighters(users):
-    midnight = time(hour=0, minute=0, microsecond=0)
-    morning = time(hour=6, minute=0, microsecond=0)
-    users_midnighters = []
+    midnight = 0
+    morning = 6
+    midnighters = []
     for user in users:
         user_time_zone = pytz.timezone(user['timezone'])
         local_dt = user_time_zone.localize(
             datetime.fromtimestamp(user['timestamp']))
+        if midnight < local_dt.hour < morning:
+            midnighters.append(user['username'])
+    return set(midnighters)
 
-        if midnight < local_dt.time() < morning:
-            users_midnighters.append(user['username'])
-    return set(users_midnighters)
+
+def print_midnighters(midnighters):
+    print('Совы на Девмане:')
+    for midnighter in midnighters:
+        print(midnighter)
 
 
 if __name__ == '__main__':
-    print(get_midnighters(load_attempts()))
+    midnighters = get_midnighters(load_attempts())
+    print_midnighters(midnighters)
